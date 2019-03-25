2a97a7b6b31b6c5017b3a33e2938f87c4abb2dbb
PoppyCurtis <poppy.curtis@gmail.com>
2019-03-25 16:41:53 +0000

    Render each day of the week through EJS using .getDay() method

--- app.js
+++ app.js
@@ -8,12 +8,9 @@ app.set('view engine', 'ejs')
 app.get("/", function(req, res) {
     var today = new Date();
     var day = "";
-    if (today.getDay() === 6 || today.getDay() === 0) {
-        day = "Weekend";
-    }
-    else {
-        day = "Weekday";
-    } 
+    var weekdays= ["Sunday", "Monday", "Tuesday", "Wednesday",
+    "Thursday", "Friday", "Saturday"];
+    day = weekdays[today.getDay()];
     res.render("list", {dayOfTheWeek: day});
 });
 
