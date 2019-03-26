f9fc8be062a30e23c5030bb83b77621ab2cbca64
PoppyCurtis <poppy.curtis@gmail.com>
2019-03-26 14:59:38 +0000

    Refactor code to make it more readable and efficient

--- app.js
+++ app.js
@@ -1,27 +1,22 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
 const items = ["Buy food", "Cook food", "Eat food"];
-let workItems = [];
+const workItems = [];
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.get("/", function (req, res) {
-    let today = new Date();
-    const options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long"
-    };
-    const day = today.toLocaleDateString("en-US", options);
+    const day = date.getDate();
     res.render("list", { listTitle: day, newListItems: items });
 });
 
 app.post("", function (req, res) {
     console.log(req.body);
-    let item = req.body.newItem;
+    const item = req.body.newItem;
     if (req.body.list === "Work") {
         workItems.push(item);
         res.redirect("/work");
