37a78200971f57f9967b7ae7473576f64a12ee05
PoppyCurtis <poppy.curtis@gmail.com>
2019-03-26 10:22:14 +0000

    Add css to design the user interface. Enable css to run on Express by using app.use(express.static()) method

--- app.js
+++ app.js
@@ -2,27 +2,27 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-var items = [];
+const items = ["Buy food", "Cook food", "Eat food"];
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
-
+app.use(express.static("public"));
 app.get("/", function(req, res) {
-    var today = new Date();
+    let today = new Date();
     
     
-    var options = {
+    const options = {
         weekday: "long",
         day: "numeric",
         month: "long"
     };
-   var day = today.toLocaleDateString("en-US", options);
+   const day = today.toLocaleDateString("en-US", options);
 
     res.render("list", {dayOfTheWeek: day, newListItems: items});
 });
 
 app.post("/", function(req, res) {
-    var item = req.body.newItem;
+    const item = req.body.newItem;
     items.push(item);
    
 
