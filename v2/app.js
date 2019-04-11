510e560811b0d39b4ef2fafc26c7773a2ff941aa
PoppyCurtis <poppy.curtis@gmail.com>
2019-04-11 22:37:15 +0100

    Create custom lists using Express Route Parameters

--- v2/app.js
+++ v2/app.js
@@ -27,6 +27,12 @@ const item3 = new Item ({
 });
 const defaultItems = [item1, item2, item3];
 
+const listSchema = {
+  name: String,
+  items: [itemSchema]
+};
+
+const List = mongoose.model("list", listSchema);
 
 app.get("/", function(req, res) {
   Item.find({}, function (err, results) {
@@ -44,23 +50,29 @@ app.get("/", function(req, res) {
     res.render("list", {listTitle: "Today", newListItems: results}
     );
   });
-
-
-  
-
 });
 
 app.post("/", function(req, res){
 
-  const item = req.body.newItem;
+  const itemName = req.body.newItem;
+  const item = new Item ({
+    name: itemName
+  })
+  item.save();
+  res.redirect("/");
+});
 
-  if (req.body.list === "Work") {
-    workItems.push(item);
-    res.redirect("/work");
-  } else {
-    //items.push(item);
-    //res.redirect("/");
-  }
+app.post("/delete", function(req, res) {
+  const checkedItemId = req.body.checkbox;
+  Item.findByIdAndRemove(checkedItemId, function(err) {
+    if (err) {
+      console.log(err);
+    }
+    else {
+      console.log("I will be deleting" + checkedItemId);
+    }
+    res.redirect("/")
+  });
 });
 
 app.get("/work", function(req,res){
@@ -71,6 +83,14 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
+app.get("/:customlist", function(req, res) {
+  const customlistname = req.params.customlist;
+  const list = new List ({
+    name: customlistname,
+    items: defaultItems
+  });
+  list.save();
+});
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
