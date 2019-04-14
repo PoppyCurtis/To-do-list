930d2b3043230686c582bc4249e7a170de4a4af9
PoppyCurtis <poppy.curtis@gmail.com>
2019-04-14 16:10:01 +0100

    Create custom lists using express routing parameters

--- v2/app.js
+++ v2/app.js
@@ -6,24 +6,24 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/todolistDB", 
-{useNewUrlParser: true});
+mongoose.connect("mongodb://localhost:27017/todolistDB",
+  { useNewUrlParser: true });
 
 const itemSchema = {
   name: String
 };
-const Item = mongoose.model("Item", itemSchema );
-const item1 = new Item ({
- name: "Welcome to your to do list!"
+const Item = mongoose.model("Item", itemSchema);
+const item1 = new Item({
+  name: "Welcome to your to do list!"
 });
-const item2 = new Item ({
- name: "Hit the + button to add a new item."
+const item2 = new Item({
+  name: "Hit the + button to add a new item."
 });
-const item3 = new Item ({
- name: "<-- Hit this to delete an item."
+const item3 = new Item({
+  name: "<-- Hit this to delete an item."
 });
 const defaultItems = [item1, item2, item3];
 
@@ -34,10 +34,10 @@ const listSchema = {
 
 const List = mongoose.model("list", listSchema);
 
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
   Item.find({}, function (err, results) {
     if (results.length === 0) {
-      Item.insertMany(defaultItems, function(err) {
+      Item.insertMany(defaultItems, function (err) {
         if (err) {
           console.log(err);
         }
@@ -47,24 +47,24 @@ app.get("/", function(req, res) {
         }
       });
     }
-    res.render("list", {listTitle: "Today", newListItems: results}
+    res.render("list", { listTitle: "Today", newListItems: results }
     );
   });
 });
 
-app.post("/", function(req, res){
+app.post("/", function (req, res) {
 
   const itemName = req.body.newItem;
-  const item = new Item ({
+  const item = new Item({
     name: itemName
   })
   item.save();
   res.redirect("/");
 });
 
-app.post("/delete", function(req, res) {
+app.post("/delete", function (req, res) {
   const checkedItemId = req.body.checkbox;
-  Item.findByIdAndRemove(checkedItemId, function(err) {
+  Item.findByIdAndRemove(checkedItemId, function (err) {
     if (err) {
       console.log(err);
     }
@@ -75,22 +75,27 @@ app.post("/delete", function(req, res) {
   });
 });
 
-app.get("/work", function(req,res){
-  res.render("list", {listTitle: "Work List", newListItems: workItems});
-});
 
-app.get("/about", function(req, res){
-  res.render("about");
-});
-
-app.get("/:customlist", function(req, res) {
+app.get("/:customlist", function (req, res) {
   const customlistname = req.params.customlist;
-  const list = new List ({
-    name: customlistname,
-    items: defaultItems
-  });
-  list.save();
+  List.findOne({ name: customlistname }, function (err, customlistname) {
+    if (!err) {
+      if (!customlistname) {
+        //Create a new list
+        const list = new List({
+          name: customlistname,
+          items: defaultItems
+        });   
+        list.save();
+      } else {
+        //show an existing list
+        res.render("list", { listTitle: customlistname.name, newListItems: customlistname.items});
+      }
+    }
+ 
+
+  })
 });
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
